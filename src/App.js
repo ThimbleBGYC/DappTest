7c635dce81e9d8e8233d3fd16edda06643913668
Thimble <99198921+ThimbleBGYC@users.noreply.github.com>
2022-06-28 17:20:22 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -238,8 +238,8 @@ function App() {
         </StyledImg3>
         </StyledLink>
       </s.Container>
+        <StyledLink target=["_blank"] href="https://boardgameyawnclub.xyz/">
         <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
-          href="https://boardgameyawnclub.xyz/"
         <s.SpacerLarge />
         <StyledLogo alt={"logo"} src={"/config/images/MINTING DAPP.png"} />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
